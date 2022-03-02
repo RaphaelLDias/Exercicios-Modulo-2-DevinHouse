cb64be9f90c70471d647c4eda1ab5348a781dc21
unknown <rdias.bio@gmail.com>
2022-03-02 14:55:28 -0300

    Atualizando

---
+++ [M2S02] - Ex 3 - Nome completo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é seu nome?");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Qual é seu sobrenome?");
+            var sobrenome = Console.ReadLine();
+            Console.WriteLine($"Seu nome completo é {nome} {sobrenome}.");
+        }
+    }
+}
---
+++ [M2S02] - Ex 4 - Variáveis parte 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var nota1 = 8;
+            var nota2 = 5;
+            var nota3 = 6;
+
+            var media = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine($"O resultado é {media}.");
+        }
+    }
+}
