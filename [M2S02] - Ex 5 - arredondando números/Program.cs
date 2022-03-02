248d25251f3ad2fa819f540180a83e1173f8e6ba
unknown <rdias.bio@gmail.com>
2022-03-02 16:15:47 -0300

    Add NewFile

---
+++ [M2S02] - Ex 5 - arredondando números/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArredondandoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número com duas casas após a virgula:");
+        }
+    }
+}
---
+++ [M2S02] - Ex 5 - arredondando números/README.md
@@ -0,0 +1,5 @@
+Descrição do Exercício:
+
+Salve dev, blz?
+
+Crie um programa que pergunte um número com 2 casas depois da vírgula, converta esse número para um inteiro e escreva o resultado na tela.
