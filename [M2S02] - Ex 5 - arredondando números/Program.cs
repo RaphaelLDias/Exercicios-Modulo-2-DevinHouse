84b4e7af270b201d7583675e70238fda222d8600
unknown <rdias.bio@gmail.com>
2022-03-03 19:26:45 -0300

    Add lines

--- [M2S02] - Ex 5 - arredondando números/Program.cs
+++ [M2S02] - Ex 5 - arredondando números/Program.cs
@@ -7,6 +7,10 @@ namespace ArredondandoNumeros
         static void Main(string[] args)
         {
             Console.WriteLine("Digite um número com duas casas após a virgula:");
+            var numero = Console.ReadLine();
+            var converteNumero = float.Parse(numero);
+            
+            Console.WriteLine($"O número arredondado é {numero}.");
         }
     }
 }
